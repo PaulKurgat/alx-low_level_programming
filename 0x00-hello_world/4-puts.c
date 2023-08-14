edf913341bf934b25654aae3257cb5b5bf0bc857
PaulKurgat <kurgatpaul7@gmail.com>
2023-08-14 05:30:36 -0700

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-* main - entry point
-* return: always 0 (Success)
-*/
+ * main - entry point
+ * return: always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
