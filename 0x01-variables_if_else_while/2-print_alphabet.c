09a98bab4acc7bf20d71b54a0adf4766ee77e74a
PaulKurgat <kurgatpaul7@gmail.com>
2023-08-16 14:27:14 -0700

    Lower Upper

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,10 +12,10 @@ int main(void)
 	char c;
 
 	c = 'a';
-	while 
+	while
 		(c <= 'z') {
 			putchar (c);
-				c++;
+			c++;
 		}
 	putchar ('\n');
 	return (0);
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* Arranging alphabet in lowercas then upper */
+
+/**
+ * main - start point
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+        char c;
+
+	char x;
+
+        c = 'a';
+
+	c = 'A';
+	while
+                (c <= 'z') {
+                        putchar (c);
+                        c++;
+                }
+	while
+                (c <= 'Z') {
+                        putchar (c);
+                        c++;
+		}
+        putchar ('\n');
+        return (0);
+}
