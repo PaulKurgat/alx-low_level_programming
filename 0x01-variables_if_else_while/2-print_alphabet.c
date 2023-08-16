6c9133f4f83c5d9350fccbcf4adf5f4a048c9461
PaulKurgat <kurgatpaul7@gmail.com>
2023-08-16 13:51:36 -0700

    Alphabet lower

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* Arrnaging alphabet in lowercase */
+
+/**
+ * main - start point
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while 
+		(c <= 'z') {
+			putchar (c);
+				c++;
+		}
+	putchar ('\n');
+	return (0);
+}
