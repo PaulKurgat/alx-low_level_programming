8941dcfc6b7c47a9598c5a08cac71960d2013b69
PaulKurgat <kurgatpaul7@gmail.com>
2023-08-16 18:11:48 -0700

    Print

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,25 @@
-fwffw
+#include <stdio.h>
+
+/**
+ * Printing all base sixteen numbers
+ * main - Start point
+ * return: always 0 (success)
+ */
+
+int main (void) 
+{
+	int p;
+	int k;
+	
+	for(p = 48; p >= 57; p++);
+		{
+			putchar(p);
+		}
+
+	for(k = 97; k >= 102; k++);
+		{
+			putchar(k);
+		}
+	putchar('\n');
+	return (0);
+}
