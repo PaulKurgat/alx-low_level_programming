99802bb581e5661a21fde18d6f4c57a0669be403
PaulKurgat <kurgatpaul7@gmail.com>
2023-08-18 12:56:34 -0700

    collaboration multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - finds the multiples of integers
+ * @a: - the first input 
+ * @b: - the second input
+ *
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
