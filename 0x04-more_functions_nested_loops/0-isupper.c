493f5f9ead353f31ee827f47f4b7754028cb0ce4
PaulKurgat <kurgatpaul7@gmail.com>
2023-08-18 11:17:30 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,16 @@
 /**
  * This fucntion checks lower case
  * c: The input to be checked
- * result: 1 for capital and 0 for lower case
+ *
+ * return: 1 for capital and 0 for lower case
  */
 
 int _isupper(int c)
+
 {
-if (c >=65 && c <= 90)
+if (c >= 65 && c <= 90)
+{
 return (1);
-else
+}
 return (0);
 }
