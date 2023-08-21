cc710be19357b41fe9bb9470c5d5358a8c52cc82
PaulKurgat <kurgatpaul7@gmail.com>
2023-08-21 04:36:40 -0700

    Reset NinetyEight

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes pointer to integer and updates value it points to
+ * ninety eight
+ * @n: input
+ * Result: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
