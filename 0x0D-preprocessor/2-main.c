ea9eef9a3db1f935554d7d987f53292ba633d7f1
PaulKurgat <kurgatpaul7@gmail.com>
2023-09-12 12:49:53 -0700

    Two main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * 2-main - defines file name wgere programme was compiled from
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n",__FILE__);
+	return(0);
+}
