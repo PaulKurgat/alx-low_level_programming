36c4ad049abec2cb4b6a3552ed54a8af4bce9895
PaulKurgat <kurgatpaul7@gmail.com>
2023-09-12 19:59:24 -0700

    Fur c

---
+++ 0x0E-structures_typedef/4-main.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: length of the string evaluated
+ */
+
+int_strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+
