f95fdb9fb49daf98f4999da2e5008d0e69d57c0c
PaulKurgat <kurgatpaul7@gmail.com>
2023-09-12 19:06:39 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines a dog on three terms
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: its owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t -typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+#endif
